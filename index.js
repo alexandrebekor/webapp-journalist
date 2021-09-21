dab21d22f2c990c3abcaf47f81ca9ed53528315b
Alexandre Bekor <contato@alexandrebekor.com>
2021-09-21 18:42:36 -0300

    createInitialUser

--- index.js
+++ index.js
@@ -23,9 +23,24 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+const userModel = require('./models/user')
+const createInitialUser = async () => {
+    const totalUsers = await userModel.count({ username: 'bekor' })
+    if(totalUsers === 0){
+        const user = userModel({ username: 'bekor', password: '12345' })
+        try {
+            await user.save()
+            console.log('added')
+        } catch(e) {
+            console.log(e)
+        }
+    }
+}
+
 mongoose
     .connect(mongo)
     .then(() => {
+        createInitialUser()
         app.listen(port, () => console.log('Server running on port ' + port))
     })
     .catch(e => console.log(e))
